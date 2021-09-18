9f709314765102acaacd5c4b1dca3406dd14d683
1241055166 <>
2021-09-18 23:49:22 +0800

    根据MybatisPlus自定义模板，生成相应的代码

---
+++ src/main/java/con/henry/BuilderApplication.java
@@ -0,0 +1,13 @@
+package con.henry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuilderApplication.class, args);
+	}
+
+}
